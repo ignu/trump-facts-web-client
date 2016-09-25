609c4b35826af207c2be3fb3046668f63abf8058
Len Smith <len@barrison.com>
2016-09-25 19:16:47 -0400

    uls

--- src/App.js
+++ src/App.js
@@ -3,39 +3,67 @@ import './App.css';
 import facts from 'trump-facts';
 import R from 'ramda';
 
-console.log('facts', facts);
-
 class HtmlFact extends Component {
   render() {
-    console.log('this.props', this.props);
     return(
-      <span>
+      <li>
         <a href={this.props.url}>{this.props.title}</a>
-        <span className="comma">, </span>
-      </span>
+      </li>
     );
   }
 }
 
+class BoardFact extends Component {
+  render() {
+    return(
+        <span>
+          [url={this.props.url}]{this.props.title}[/url]
+          { this.props.showComma && <span className="comma">, </span> }
+        </span>
+    );
+  }
+}
 
 class App extends Component {
-  renderFacts() {
-    return R.map((f) => <HtmlFact url={f.urls[0]} title={f.title}/>)(facts.data.crazy)
+  renderList(data) {
+    const first = R.init(data);
+    const last = R.last(data);
+    console.log('first', first);
+    console.log('last', last);
+    let toHtml = (showComma) => {
+      return (f) => <HtmlFact showComma={showComma} url={f.urls[0]} title={f.title}/>;
+    }
+    let html = R.map(toHtml(true))(first);
+    html.push(toHtml(false)(last));
+    return html;
+  }
+
+  renderMessageBoard() {
+    //let data = facts.data.crazy;
+
+    //return R.map((f) => <BoardFact url={f.urls[0]} title={f.title}/>)(data);
   }
 
   render() {
-
-
     return (
       <div className="App">
         <div className="App-header">
           <h1>Trump Facts</h1>
         </div>
         <p className="App-intro">
+          <h3>Donald Trump is literally insanse</h3>
           <div>
-            <span>Donald Trump believes </span>
-            { this.renderFacts()}
+            <span>Donald Trump is an idiot who believes </span>
+            <ul>{ this.renderList(facts.data.crazy)}</ul>
           </div>
+
+          <div>
+            <h3>Donald Trump is a literal fascist</h3>
+            <span>Donald Trump is a madman who </span>
+            <ul>{ this.renderList(facts.data.fascist)}</ul>
+          </div>
+        <div>
+      </div>
         </p>
       </div>
     );
