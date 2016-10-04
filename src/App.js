e7242c34f048f5b08d77e2022a34140a869fd7a8
Len Smith <len@barrison.com>
2016-10-03 21:29:34 -0700

    Styling.. move to text based layout

--- src/App.js
+++ src/App.js
@@ -3,11 +3,24 @@ import './App.css';
 import facts from 'trump-facts';
 import R from 'ramda';
 
+let citationCount = 0;
+let citations = [];
+
 class HtmlFact extends Component {
   render() {
+    let printCitations = (url) => {
+      citationCount++
+      citations.push(<li>[{citationCount}] {url}</li>);
+
+      return <span> <a href={url} class="citation"><sup>[{citationCount}]</sup></a></span>
+    }
+
     return(
       <li>
-        <a href={this.props.url}>{this.props.title}</a>
+        <a href={this.props.url}>* {this.props.title}</a>
+        {
+          R.map(printCitations)(this.props.urls)
+        }
       </li>
     );
   }
@@ -29,7 +42,7 @@ class App extends Component {
     const first = R.init(data);
     const last = R.last(data);
     let toHtml = (showComma) => {
-      return (f) => <HtmlFact showComma={showComma} url={f.urls[0]} title={f.title}/>;
+      return (f) => <HtmlFact showComma={showComma} urls={f.urls} url={f.urls[0]} title={f.title}/>;
     }
     let html = R.map(toHtml(true))(first);
     html.push(toHtml(false)(last));
@@ -46,26 +59,36 @@ class App extends Component {
     return (
       <div className="App">
         <div className="App-header">
-          <h1>Trump is a...</h1>
+        <h1>Donald Trump is a <a href="#fascist">fascist</a> <a href="#idiot">idiot</a> <a href="#sexist">sexist</a> <a href="#liar">liar</a></h1>
         </div>
         <p className="App-intro">
+        
           <div>
+            <a name="fascist"></a>
             <h3>Donald Trump is a fascist who</h3>
             <ul>{ this.renderList(facts.data.fascist)}</ul>
           </div>
 
-          <h3>Donald Trump is an idiot who believes</h3>
-
           <div>
+            <a name="idiot"></a>
+            <h3>Donald Trump is an idiot who believes</h3>
             <ul>{ this.renderList(facts.data.crazy)}</ul>
           </div>
 
-          <h3>Donald Trump is a liar who...</h3>
-
           <div>
-            <ul>{ this.renderList(facts.data.crazy)}</ul>
+            <a name="liar"></a>
+            <h3>Donald Trump is a liar who...</h3>
+            <ul>{ this.renderList(facts.data.liar)}</ul>
           </div>
+
         <div>
+          <a name="liar"></a>
+          <h3>Donald Trump is a sexist who...</h3>
+          <ul>{ this.renderList(facts.data.sexist)}</ul>
+        </div>
+
+        <div>
+        <ul>{citations}</ul>
       </div>
         </p>
       </div>
